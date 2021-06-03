2bd809b9afadbb4d7e449647f22142c64e1977c1
seyi egunjobi <seyistry@gmail.com>
2021-06-03 23:25:33 +0100

    List all deck

---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from "react-native";
+
+export class DeckList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Decks List</Text>
+            </View>
+        )
+    }
+}
+
+export default DeckList
