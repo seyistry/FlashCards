c9d5647be7d650938a40a1f1b65286d07f5a780a
seyi egunjobi <seyistry@gmail.com>
2021-06-03 23:25:10 +0100

    Component to add new Deck

---
+++ components/AddDeck.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text} from "react-native";
+
+
+export class AddDeck extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Add Decks</Text>
+            </View>
+        )
+    }
+}
+
+export default AddDeck
