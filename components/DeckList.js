aae146c792d2127cb446b612d8d9f1f276d34ba6
seyi egunjobi <seyistry@gmail.com>
2021-06-04 01:35:37 +0100

    components now load intial data

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { ScrollView, View, StyleSheet, Text } from "react-native";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/index";
+import Decks from './Decks'
 
 export class DeckList extends Component {
+    componentDidMount () {
+        this.props.handleInitialData();
+    }
     render() {
+        const data = this.props.decks;
         return (
-            <View>
-                <Text>Decks List</Text>
-            </View>
-        )
+            <ScrollView>
+                {Object.keys(data).map((title) => (
+                    <Decks title={title} key={title} questions={data[title].questions} />
+                ))}
+            </ScrollView>
+        );
     }
 }
 
-export default DeckList
+function mapStateToProps(state) {
+    return {
+        decks: state,
+    };
+}
+
+export default connect(mapStateToProps, { handleInitialData })(DeckList);
