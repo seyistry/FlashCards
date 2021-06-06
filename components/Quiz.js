7c13656bd980eff5881d3fd4502ba93996931502
seyi egunjobi <seyistry@gmail.com>
2021-06-06 17:44:41 +0100

    Remove pageViewer. Quiz now update scores and shows result

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,33 +1,122 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import {
+    View,
+    Platform,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Button,
+} from "react-native";
 import { connect } from "react-redux";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import PagerView from "react-native-pager-view";
 
-export class Quiz extends Component {
-    render() {
-        return (
-            <PagerView style={styles.pagerView} initialPage={0}>
-                <View key="1">
-                    <Text>First page</Text>
-                </View>
-                <View key="2">
-                    <Text>Second page</Text>
-                </View>
-            </PagerView>
-        );
-    }
+function SubmitBtn({ text, onPress }) {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <Text>{text}</Text>
+        </TouchableOpacity>
+    );
 }
 
-const styles = StyleSheet.create({
-    pagerView: {
-        flex: 1,
-    },
-});
+const Quiz = (props) => {
+    const [index, setIndex] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [showAnswer, setShowAnwser] = useState(false);
+    const { questions } = props;
+    const totalCount = questions.length;
 
-function mapStateToProps(decks) {
+    handleAnswer = (text) => {
+        setIndex(index + 1);
+        setCorrect(text === "correct" ? correct + 1 : correct);
+        setShowAnwser(false);
+    };
+
+    goBackToDeck = () => {
+        props.goBack();
+    };
+
+    handleShowAnswer = () => {
+        setShowAnwser(!showAnswer);
+    };
+
+    handleRestart = () => {
+        setIndex(0);
+        setCorrect(0);
+        setShowAnwser(false);
+    };
+
+    return (
+        <View>
+            {index < totalCount ? (
+                showAnswer ? (
+                    <View>
+                        <Text>
+                            [{index + 1}/{totalCount}]
+                        </Text>
+                        <Text>Answer : {questions[index].answer}</Text>
+                        <Button
+                            title="Show Question"
+                            color="#7daed3"
+                            onPress={handleShowAnswer}
+                        >
+                            Show Question
+                        </Button>
+                        <SubmitBtn
+                            text="Correct"
+                            onPress={() => handleAnswer("correct")}
+                        />
+                        <SubmitBtn
+                            text="Incorrect"
+                            onPress={() => handleAnswer("incorrect")}
+                        />
+                    </View>
+                ) : (
+                    <View>
+                        <Text>
+                            [{index + 1}/{totalCount}]
+                        </Text>
+                        <Text>{questions[index].question}</Text>
+                        <Button
+                            title="Show Answer"
+                            color="#7daed3"
+                            onPress={handleShowAnswer}
+                        >
+                            Show Answer
+                        </Button>
+                        <SubmitBtn
+                            text="Correct"
+                            onPress={() => handleAnswer("correct")}
+                        />
+                        <SubmitBtn
+                            text="Incorrect"
+                            onPress={() => handleAnswer("incorrect")}
+                        />
+                    </View>
+                )
+            ) : (
+                <View>
+                    <Text>You have scored</Text>
+                    <Text>
+                        {correct} out of {totalCount}
+                    </Text>
+                    <SubmitBtn text="Restart Quiz" onPress={handleRestart} />
+                    <SubmitBtn text="Back to Deck" onPress={goBackToDeck} />
+                </View>
+            )}
+        </View>
+    );
+};
+
+function mapStateToProps(decks, { route }) {
+    const { title } = route.params;
     return {
-        decks,
+        questions: decks[title].questions,
     };
 }
 
-export default connect(mapStateToProps)(Quiz);
+function mapDispatchToProps( dispatch ,{ navigation }) {
+    return {
+        goBack: () => navigation.goBack(),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
